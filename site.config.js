a29c5b053ec06b50c41d22f2ed13bdc673196304
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-10-18 14:51:03 +0700

    Initial commit

---
+++ site.config.js
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: `@whoisryosuke`,
+  title: `NextJS MDX Deck`,
+  date: `April 22, 2020`,
+  author: {
+    url: `https://twitter.com/whoisryosuke`,
+  },
+}
+
+export default siteConfig
